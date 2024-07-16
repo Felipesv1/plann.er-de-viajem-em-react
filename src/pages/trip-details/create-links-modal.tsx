8dd6203442e1557220e990622ecfac3a481ce946
Felipesv1 <fepereirasilva4@gmail.com>
2024-07-16 15:09:47 -0300

    fix: type button remove

--- src/pages/trip-details/create-links-modal.tsx
+++ src/pages/trip-details/create-links-modal.tsx
@@ -1,26 +1,26 @@
 import { Link2, Tag, X } from "lucide-react";
-// import { FormEvent } from "react";
+import { FormEvent } from "react";
 import { Button } from "../../components/button";
-// import { useParams } from "react-router-dom";
-// import { api } from "../../lib/axios";
+import { useParams } from "react-router-dom";
+import { api } from "../../lib/axios";
 
 interface saveLinksProps {
   closeLinkModal: () => void;
 }
 export function CreateLinksModal({ closeLinkModal }: saveLinksProps) {
-  //   const { tripId } = useParams();
+  const { tripId } = useParams();
 
-  //   async function createLink(event: FormEvent<HTMLFormElement>) {
-  //     event.preventDefault();
-  //     const data = new FormData(event.currentTarget);
-  //     const title = data.get("title")?.toString();
-  //     const url = data.get("url")?.toString();
-  //     await api.post(`/trips/${tripId}/links`, {
-  //       title,
-  //       url,
-  //     });
-  //     window.document.location.reload();
-  //   }
+  async function createLink(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const title = data.get("title")?.toString();
+    const url = data.get("url")?.toString();
+    await api.post(`/trips/${tripId}/links`, {
+      title,
+      url,
+    });
+    window.document.location.reload();
+  }
   return (
     <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
       <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
@@ -35,7 +35,7 @@ export function CreateLinksModal({ closeLinkModal }: saveLinksProps) {
             Todos convidados podem visualizar os links importantes.
           </p>
         </div>
-        <form className="space-y-3">
+        <form onSubmit={createLink} className="space-y-3">
           <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
             <Tag className="text-zinc-400 size-5" />
             <input
@@ -53,7 +53,7 @@ export function CreateLinksModal({ closeLinkModal }: saveLinksProps) {
               className="bg-transparent text-lg placeholder:-zinc-400 outline-none flex-1"
             />
           </div>
-          <Button type="submit" variant="primary" size="full">
+          <Button variant="primary" size="full">
             Salvar link
           </Button>
         </form>
